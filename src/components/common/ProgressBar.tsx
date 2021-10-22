f2865e2c1167e56d2259ea1356e264cb7e7f8a86
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-10-22 17:09:44 +0900

    Feat: Survey > ProgressBar 업로드

---
+++ src/components/common/ProgressBar.tsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import styles from './ProgressBar.module.scss';
+
+const { s_wrapper, s_step, s_progress, s_currentStep } = styles;
+
+function ProgressBar({ percent }: { percent: number }): JSX.Element {
+  const [value, setValue] = useState(percent * 312 - 60);
+
+  useEffect(() => {
+    setValue(percent * 312);
+  }, [percent]);
+
+  return (
+    <div className={s_wrapper}>
+      <div style={{ width: `${value}px` }} className={s_progress} />
+      <span className={s_step}>
+        <span className={s_currentStep}>{percent * 5}</span> / 5 step
+      </span>
+    </div>
+  );
+}
+
+export default ProgressBar;
