bbaa2cb65b9f2cc728c556ccec62190c42e2f377
HaJunRyu <hjr4557@naver.com>
2021-10-22 10:50:30 +0900

    Modify: 사용하지 않는 div 제거
    Changes to be committed:
    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,14 @@ import { Home, Survey, ExerciseChoice, ExerciseRoutine, NotFound } from '@/pages
 
 function App(): ReactElement {
   return (
-    <div>
-      <Switch>
-        <Route exact path={HOME_PAGE} component={Home} />
-        <Route path={SURVEY_PAGE} component={Survey} />
-        <Route path={EXERCISE_CHOICE_PAGE} component={ExerciseChoice} />
-        <Route path={EXERCISE_ROUTINE_PAGE} component={ExerciseRoutine} />
-        <Route path={NOT_FOUND_PAGE} component={NotFound} />
-        <Redirect to={NOT_FOUND_PAGE} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path={HOME_PAGE} component={Home} />
+      <Route path={SURVEY_PAGE} component={Survey} />
+      <Route path={EXERCISE_CHOICE_PAGE} component={ExerciseChoice} />
+      <Route path={EXERCISE_ROUTINE_PAGE} component={ExerciseRoutine} />
+      <Route path={NOT_FOUND_PAGE} component={NotFound} />
+      <Redirect to={NOT_FOUND_PAGE} />
+    </Switch>
   );
 }
 
