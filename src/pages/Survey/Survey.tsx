465304dca056725fc770481cde4fe8d7fd1082d6
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-10-24 00:15:50 +0900

    Feat: Survey Page 추가

--- src/pages/Survey/Survey.tsx
+++ src/pages/Survey/Survey.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styles from './survey.module.scss';
+import useForm from '../../hooks/useForm';
+import SurveyComponent from './SurveyComponent';
+import { SURVEY_INITIALSTATE } from '@/consts/state';
 
-function Survey() {
-  return <div />;
-}
+const Survey = (): any => {
+  const { currentPage, surveyState, setState, setPageCount } = useForm({
+    SURVEY_INITIALSTATE,
+  });
+
+  const components = SurveyComponent({ surveyState, setState, setPageCount });
+
+  return components.map((component, page) => {
+    if (currentPage === page) {
+      return (
+        <div className={styles.container} key={`component-${currentPage}`}>
+          <>{component}</>
+        </div>
+      );
+    }
+    return null;
+  });
+};
 
 export default Survey;
