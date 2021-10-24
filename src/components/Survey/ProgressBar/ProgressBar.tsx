d6fae4b56b45b2fb6cf09cdf61ff5e7ca0e9c8a7
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-10-24 15:46:07 +0900

    Feat: Props 분리 & State 이름 변경(value -> progress)

--- src/components/Survey/ProgressBar/ProgressBar.tsx
+++ src/components/Survey/ProgressBar/ProgressBar.tsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import styles from './ProgressBar.module.scss';
 
+interface Props {
+  percent: number;
+}
+
 const { s_wrapper, s_step, s_progress, s_currentStep } = styles;
 
-const ProgressBar = ({ percent }: { percent: number }) => {
-  const [value, setValue] = useState(percent * 312 - 60);
+const ProgressBar = ({ percent }: Props) => {
+  const [progress, setProgress] = useState(percent * 312 - 60);
 
   useEffect(() => {
-    setValue(percent * 312);
+    setProgress(percent * 312);
   }, [percent]);
 
   return (
     <div className={s_wrapper}>
-      <div style={{ width: `${value}px` }} className={s_progress} />
+      <div style={{ width: `${progress}px` }} className={s_progress} />
       <span className={s_step}>
         <span className={s_currentStep}>{percent * 5}</span> / 5 step
       </span>
