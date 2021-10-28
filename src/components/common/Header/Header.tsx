d6bcd704770951556aadd077300629dfc1a2fda5
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-10-28 23:21:38 +0900

    Feat: Header Component 추상화

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import classNames from 'classnames';
-import styles from './Hedaer.module.scss';
+import styles from './Header.module.scss';
 
 const { s_header, s_previous__button } = styles;
 
 interface Props {
-  currentPage: number;
-  setPageCount: (page: number) => void;
+  handleClickBackButton: () => void;
 }
 
-const Header = ({ currentPage, setPageCount }: Props) => (
+const Header = ({ handleClickBackButton }: Props) => (
   <header className={classNames(s_header)}>
     <button
       type="button"
       className={classNames(s_previous__button)}
-      onClick={() => setPageCount(currentPage - 1)}
+      onClick={handleClickBackButton}
     >
       <ArrowBackIcon />
     </button>
