49f6ec4844de07e6ce49ca73d2dc407de2944126
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-10-31 21:24:17 +0900

    Feat: 설문조사 시작 & 끝에 텍스트 추가

--- src/components/Survey/ProgressBar/ProgressBar.tsx
+++ src/components/Survey/ProgressBar/ProgressBar.tsx
@@ -1,28 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import styles from './ProgressBar.module.scss';
+import styles from './progressBar.module.scss';
 
 interface Props {
-  percent: number;
+  step: number;
 }
 
-const { s_wrapper, s_step, s_progress, s_currentStep } = styles;
+const { s_container, s_stepContainer, s_stepComment, s_progress, s_currentStep } = styles;
 
 const TOTAL_LENGTH = 312;
 const TOTAL_STEP = 6;
 
-const ProgressBar = ({ percent }: Props) => {
-  const [progress, setProgress] = useState(percent * TOTAL_LENGTH - TOTAL_STEP);
+const ProgressBar = ({ step }: Props) => {
+  const [progress, setProgress] = useState(step * TOTAL_LENGTH - TOTAL_LENGTH / TOTAL_STEP);
 
   useEffect(() => {
-    setProgress((percent * TOTAL_LENGTH) / TOTAL_STEP);
-  }, [percent]);
+    setProgress((step * TOTAL_LENGTH) / TOTAL_STEP);
+  }, [step]);
 
   return (
-    <div className={s_wrapper}>
+    <div className={s_container}>
       <div style={{ width: `${progress}px` }} className={s_progress} />
-      <span className={s_step}>
-        <span className={s_currentStep}>{percent * 5}</span> / 5 step
-      </span>
+      <div className={s_stepContainer}>
+        <span className={s_stepComment}>
+          {step === 1 && '시작이 반!'}
+          {step === 6 && '얼마남지 않았어요!'}
+        </span>
+        <div className={s_currentStep}>
+          <span>{step}</span> / {TOTAL_STEP}step
+        </div>
+      </div>
     </div>
   );
 };
