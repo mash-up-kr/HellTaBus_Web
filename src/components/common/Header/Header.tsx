5a442f424bcc1656d737f7206fa73ce9e488d3c3
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-10-31 21:30:59 +0900

    Chore: MUI Package 제거

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import classNames from 'classnames';
-import styles from './Header.module.scss';
+import styles from './header.module.scss';
 
-const { s_header, s_previous__button } = styles;
+const { s_header, s_previousButton } = styles;
 
 interface Props {
   handleClickBackButton: () => void;
 }
 
-const Header = ({ handleClickBackButton }: Props) => (
-  <header className={classNames(s_header)}>
-    <button
-      type="button"
-      className={classNames(s_previous__button)}
-      onClick={handleClickBackButton}
-    >
-      <ArrowBackIcon />
-    </button>
-  </header>
-);
+function Header({ handleClickBackButton }: Props) {
+  return (
+    <header className={s_header}>
+      <button type="button" className={s_previousButton} onClick={handleClickBackButton}>
+        뒤로 가기
+      </button>
+    </header>
+  );
+}
 
 export default Header;
