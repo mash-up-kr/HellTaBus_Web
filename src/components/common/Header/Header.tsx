86caf3dc0993fbf8150ae27b5dc3ee4a6e8fe424
Kim-Jaemin420 <jany240@naver.com>
2021-10-31 22:49:12 +0900

    feat: survey page

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './header.module.scss';
+import Back from '@/assets/back.svg';
+
+const { s_header, s_previousButton } = styles;
+
+interface Props {
+  handleClickBackButton: () => void;
+}
+
+function Header({ handleClickBackButton }: Props) {
+  return (
+    <header className={classNames(s_header)}>
+      <button
+        type="button"
+        className={classNames(s_previousButton)}
+        onClick={handleClickBackButton}
+      >
+        <Back />
+      </button>
+    </header>
+  );
+}
+
+export default Header;
