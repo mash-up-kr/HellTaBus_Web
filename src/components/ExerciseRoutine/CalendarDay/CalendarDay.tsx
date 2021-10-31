9ed35d751963ee728dde1bef87fa6aa3e0308786
HaJunRyu <hjr4557@naver.com>
2021-10-31 23:04:52 +0900

    Feat: 주간 기록의 Calendar 컴포넌트에 쓰일 CalendarDay 컴포넌트 정의
    Changes to be committed:
    new file:   src/components/ExerciseRoutine/CalendarDay/CalendarDay.tsx
    new file:   src/components/ExerciseRoutine/CalendarDay/calendarDay.module.scss

---
+++ src/components/ExerciseRoutine/CalendarDay/CalendarDay.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames';
+import style from './calendarDay.module.scss';
+
+interface Props {
+  day: string;
+  date: number;
+  didExercise: boolean;
+}
+
+const { s_calendarDay, s_day, s_date, s_today, s_didExercise } = style;
+
+const CalendarDay = ({ day, date, didExercise }: Props) => {
+  const isToday = new Date().getDate() === date;
+  return (
+    <li className={classNames(s_calendarDay, didExercise && s_didExercise, isToday && s_today)}>
+      <button type="button">
+        <span className={s_day}>{day}</span>
+        <span className={s_date}>{date}</span>
+      </button>
+    </li>
+  );
+};
+
+export default CalendarDay;
