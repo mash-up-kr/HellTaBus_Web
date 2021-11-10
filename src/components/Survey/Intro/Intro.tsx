b677955ccd5f2cb4cecc1b72bb7c84d8b0035f03
Kim-Jaemin420 <jany240@naver.com>
2021-11-10 18:14:26 +0900

    Feat: intro 컴포넌트 생성

--- src/components/Survey/Intro/Intro.tsx
+++ src/components/Survey/Intro/Intro.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Lottie from 'react-lottie';
 import classNames from 'classnames';
 import style from './intro.module.scss';
-import Hand from '@/assets/lottie/hand-habimetro.json';
+import Welcome from '@/assets/lottie/hand-habimetro.json';
 import { CommonButton } from '@/components/common';
 
 interface Props {
@@ -13,19 +13,14 @@ const { s_a11yHidden, s_container, s_lottieContainer, s_title, s_addendum } = st
 
 const Intro = ({ handleClickButton }: Props) => {
   const lottieOptions = {
-    animationData: Hand,
-    loop: true,
-    autoplay: true,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
+    animationData: Welcome,
   };
 
   return (
     <section className={classNames(s_container)}>
       <h1 className={classNames(s_a11yHidden)}>설문조사 시작</h1>
       <div className={classNames(s_lottieContainer)}>
-        <Lottie options={lottieOptions} isClickToPauseDisabled={false} width={144} height={144} />
+        <Lottie options={lottieOptions} width={144} height={144} />
       </div>
       <div className={classNames(s_title)}>
         <span>딱! 맞는 추천 서비스</span>를 위해 몇 가지만 물어볼게요.
