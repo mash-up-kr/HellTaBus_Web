f1abeb153f5ec780f2153e2171b0d44980636e4d
HaJunRyu <hjr4557@naver.com>
2021-11-10 18:30:11 +0900

    Feat: RecommendCarousel의 아이템들이 스크롤전에 2.3개만 보이게끔 변경
    Changes to be committed:
    modified:   src/components/ExerciseRoutine/ExerciseCard/ExerciseCard.tsx
    modified:   src/components/ExerciseRoutine/ExerciseCard/exerciseCard.module.scss

--- src/components/ExerciseRoutine/ExerciseCard/ExerciseCard.tsx
+++ src/components/ExerciseRoutine/ExerciseCard/ExerciseCard.tsx
@@ -6,13 +6,13 @@ interface Props {
   exerciseName: string;
 }
 
-const { s_exercise, s_exerciseNameWraper } = style;
+const { s_exercise, s_exerciseNameWraper, s_exerciseImage } = style;
 
 const ExerciseCard = ({ imageUrl, exerciseName }: Props) => {
   return (
     <article className={s_exercise}>
       <div>
-        <img src={imageUrl} alt={exerciseName} />
+        <img src={imageUrl} alt={exerciseName} className={s_exerciseImage} />
       </div>
       <div className={s_exerciseNameWraper}>
         <span>{exerciseName}</span>
