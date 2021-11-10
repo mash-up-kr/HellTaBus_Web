1f11f1db5d82a378271fc58db8d5c68ea3be5eb9
Kim-Jaemin420 <jany240@naver.com>
2021-11-10 18:11:00 +0900

    Refactor: validation 로직 수정, 레이아웃 수정

--- src/components/Survey/Nickname/Nickname.tsx
+++ src/components/Survey/Nickname/Nickname.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import classNames from 'classnames';
 import style from './nickname.module.scss';
-import Coolicon from '@/assets/coolicon.svg';
+import ErrorIcon from '@/assets/svg/error-icon.svg';
 import NextButton from '@/components/common/NextButton/NextButton';
 
 const {
@@ -20,52 +20,66 @@ interface Props {
   setNextPage: () => void;
 }
 
-function Nickname({ nickname, setNickname, setNextPage }: Props) {
-  const [isDisabled, setIsDisabled] = useState<boolean>(!nickname);
+const Nickname = ({ nickname, setNickname, setNextPage }: Props) => {
   const [errorMessage, setErrorMessage] = useState<string>('');
+  const isDisabled = useMemo(() => !nickname || !!errorMessage, [nickname, errorMessage]);
 
-  const checkNicknameValidation = (value: string) => {
+  const isValidNickname = useCallback(() => {
+    const specialCharactersRegex = /[`~!@#$%^&*()|+\-=?;:'"<>\\{\\}\\[\]\\\\/]/g;
     const nicknameRegex = /^[가-힣\s|ㄱ-ㅎ|a-z|A-Z|0-9|_|.|,]+$/g;
     const nicknameLengthRegex = /^.{2,8}$/g;
 
-    if (!nicknameRegex.test(value)) {
-      setErrorMessage('한글, 영어, 숫자, 특수문자(_.,)만 가능해요');
-      setIsDisabled(true);
-    } else if (!nicknameLengthRegex.test(value)) {
-      setErrorMessage('2 ~ 8글자만 가능해요');
-      setIsDisabled(true);
+    if (specialCharactersRegex.test(nickname)) {
+      return '특수문자는 . , _ 만 가능해요';
+    }
+
+    if (!nicknameRegex.test(nickname)) {
+      return '한글, 영어, 숫자, 특수문자(_.,)만 가능해요';
+    }
+
+    if (!nicknameLengthRegex.test(nickname)) {
+      return '2 ~ 8글자만 가능해요';
+    }
+
+    return null;
+  }, [nickname]);
+
+  useEffect(() => {
+    const nickNameError = isValidNickname();
+
+    if (nickNameError) {
+      setErrorMessage(nickNameError);
     } else {
       setErrorMessage('');
-      setIsDisabled(false);
     }
-  };
+  }, [isValidNickname]);
 
-  // onChange
   const handleChangeNickname = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { value }: { value: string } = e.target;
     setNickname(value);
-    checkNicknameValidation(value);
   };
 
   return (
     <section className={classNames(s_container)}>
-      <h2 className={classNames(s_title)}>당신을 뭐라고 불러드릴까요?</h2>
+      <h2 className={classNames(s_title)}>
+        <p>당신을</p> 뭐라고 불러드릴까요?
+      </h2>
       <div className={classNames(s_inputContainer)}>
         <input
           className={classNames(s_commonInput, {
-            [s_errorInput]: errorMessage,
+            [s_errorInput]: nickname !== '' && errorMessage,
           })}
           type="text"
           placeholder="닉네임 입력"
           value={nickname}
           onChange={handleChangeNickname}
         />
-        {errorMessage && <Coolicon className={classNames(s_errorIcon)} />}
+        {nickname !== '' && errorMessage && <ErrorIcon className={classNames(s_errorIcon)} />}
       </div>
-      <span className={classNames(s_errorMsg)}>{errorMessage}</span>
+      <span className={classNames(s_errorMsg)}>{nickname !== '' && errorMessage}</span>
       <NextButton handleClickNextButton={setNextPage} isDisabled={isDisabled} />
     </section>
   );
-}
+};
 
 export default Nickname;
