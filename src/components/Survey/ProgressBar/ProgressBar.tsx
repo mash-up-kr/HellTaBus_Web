c77cca626d2a0701334f47236884617cb0bcc301
minsoopark <minsoopark@linecorp.com>
2021-11-13 18:52:23 +0900

    Fix: Header, ProgressBar 컴포넌트 반응형으로 동작하게끔 수정

--- src/components/Survey/ProgressBar/ProgressBar.tsx
+++ src/components/Survey/ProgressBar/ProgressBar.tsx
@@ -8,19 +8,18 @@ interface Props {
 
 const { s_container, s_stepContainer, s_stepComment, s_progress, s_currentStep } = styles;
 
-const TOTAL_LENGTH = 312;
 const TOTAL_STEP = 6;
 
 const ProgressBar = ({ step }: Props) => {
   const [progress, setProgress] = useState(0);
 
   useEffect(() => {
-    setProgress((step * TOTAL_LENGTH) / TOTAL_STEP);
+    setProgress((step / TOTAL_STEP) * 100);
   }, [step]);
 
   return (
     <div className={classNames(s_container)}>
-      <div style={{ width: `${progress}px` }} className={classNames(s_progress)} />
+      <div style={{ width: `${progress}%` }} className={classNames(s_progress)} />
       <div className={classNames(s_stepContainer)}>
         <span className={classNames(s_stepComment)}>
           {step === 1 && '시작이 반!'}
