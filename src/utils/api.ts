d1abde766bc168dba303bbd2d3277ae1034dabf8
HaJunRyu <hjr4557@naver.com>
2021-11-22 01:41:41 +0900

    Refactor: response프로퍼티의 참조값이 undefined일때의 예외처리 추가
    Changes to be committed:
    modified:   src/utils/api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,6 @@
 import { AxiosError } from 'axios';
 
 export const throwErrorMessage = (error: AxiosError) => {
-  if (error && error.response) throw error.response.data.message;
+  if (!error.response) throw error;
+  throw error.response.data.message;
 };
