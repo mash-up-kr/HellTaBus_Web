8c81c58f0ab7f293946a31d79aeecd7f2c2406a4
HaJunRyu <hjr4557@naver.com>
2021-11-24 03:38:55 +0900

    Refactor: 일요일과 토요일 날짜의 문자열 포멧을 만들어 줄 유틸함수 정의 및 사용
    Changes to be committed:
    modified:   src/hooks/page/useExerciseRoutine.ts
    new file:   src/utils/date.ts
    modified:   src/utils/index.ts

--- src/hooks/page/useExerciseRoutine.ts
+++ src/hooks/page/useExerciseRoutine.ts
@@ -1,31 +1,22 @@
 import { useFetchExerciseHistory, useFetchExerciseSuggestion } from '@/hooks/api';
+import { getFirstAndLastDateStringFromCurrentWeek } from '@/utils';
 
 const useExerciseRoutine = () => {
-  const today = new Date();
-  const currentYear = today.getFullYear();
-  const currentMonth = today.getMonth();
-  const currentDate = today.getDate();
-  const currentDay = today.getDay();
-
-  const sunday = new Date(currentYear, currentMonth, currentDate - currentDay);
-  const saturday = new Date(currentYear, currentMonth, currentDate + (6 - currentDay));
-
-  const from = `${sunday.getFullYear()}-${sunday.getMonth() + 1}-${sunday.getDate()}`;
-  const to = `${saturday.getFullYear()}-${saturday.getMonth() + 1}-${saturday.getDate()}`;
+  const { sundayDateString, saturdayDateString } = getFirstAndLastDateStringFromCurrentWeek();
 
   const {
     suggestion,
     error: suggestionError,
     isError: isSuggestionError,
     isLoading: isLoadingSuggestion,
-  } = useFetchExerciseSuggestion(from, to);
+  } = useFetchExerciseSuggestion(sundayDateString, saturdayDateString);
 
   const {
     exerciseHistory,
     error: exerciseHistoryError,
     isError: isExerciseHistoryError,
     isLoading: isLoadingExerciseHistory,
-  } = useFetchExerciseHistory(from, to);
+  } = useFetchExerciseHistory(sundayDateString, saturdayDateString);
 
   const error = suggestionError || exerciseHistoryError;
   const isError = isSuggestionError || isExerciseHistoryError;
