bec58dc09d08188c15637e19be88570dcb393633
minsoopark <minsoopark@linecorp.com>
2021-11-24 16:13:22 +0900

    Feat: ExerciseChoice 페이지에 Header, Tabs 컴포넌트 추가

--- src/pages/ExerciseChoice/ExerciseChoice.tsx
+++ src/pages/ExerciseChoice/ExerciseChoice.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import classNames from 'classnames';
 import { useExerciseChoice } from '@/hooks';
+import { Tabs, Header } from '@/components';
+import style from './exerciseChoice.module.scss';
+
+const { s_exerciseChoice } = style;
 
 function ExerciseChoice() {
-  const { isLoading, error, isError, parsedExercises } = useExerciseChoice();
-
-  if (isLoading) {
-    return <div>로딩...</div>;
-  }
+  const { isExerciseLoading, error, isError, tabHeaders } = useExerciseChoice();
 
   if (isError) {
     return <div>{error?.message}</div>;
   }
 
-  return <div />;
+  return (
+    <div className={classNames(s_exerciseChoice)}>
+      <Header title="운동변경" handleClickBackButton={() => {}} />
+      <Tabs headers={tabHeaders}>
+        {isExerciseLoading ? (
+          <div>로딩중</div>
+        ) : (
+          tabHeaders.map((tab, index) => <p key={index}>{tab.title}</p>)
+        )}
+      </Tabs>
+    </div>
+  );
 }
 
 export default ExerciseChoice;
