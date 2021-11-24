46c55dd4e53001cb1e156ba3e6d178735a9541cb
minsoopark <minsoopark@linecorp.com>
2021-11-24 15:50:41 +0900

    Feat: splitType에 따라 exerciseTabHeaders 생성하는 비즈니스 로직 추가

--- src/hooks/page/useExerciseChoice.ts
+++ src/hooks/page/useExerciseChoice.ts
@@ -1,13 +1,42 @@
-import { useFetchExercises } from '@/hooks/api';
-import { Exercise } from '@/types';
+import { useMemo } from 'react';
+import { EXERCISE_PART, EXERCISE_PART_BY_SPLIT_TYPE } from '@/consts';
+import { useFetchExercises, useFetchUserInfo } from '@/hooks';
+import { Tab } from '@/types';
+import { reduceObject } from '@/utils';
 
 const useExerciseChoice = () => {
-  const { isLoading, error, isError, exercises } = useFetchExercises();
+  const {
+    isLoading: isExerciseLoading,
+    error: exerciseError,
+    isError: isExerciseError,
+    exercises,
+  } = useFetchExercises();
+  const { error: userInfoError, isError: isUserInfoError, userInfo } = useFetchUserInfo();
 
-  const parseExercises = (list: Exercise[] | undefined) => list;
-  const parsedExercises = parseExercises(exercises);
+  const exerciseTabHeaders: Tab[] = useMemo(
+    () =>
+      userInfo?.splitType
+        ? (reduceObject(
+            EXERCISE_PART_BY_SPLIT_TYPE[userInfo?.splitType],
+            (accumulated, value, key) => {
+              if (value === EXERCISE_PART.BICEPS || value === EXERCISE_PART.TRICEPS) {
+                return accumulated;
+              }
+              return [...(accumulated as []), { id: key, title: value }];
+            },
+            []
+          ) as Tab[])
+        : [],
+    [userInfo]
+  );
 
-  return { isLoading, error, isError, parsedExercises };
+  return {
+    isExerciseLoading,
+    error: exerciseError || userInfoError,
+    isError: isExerciseError || isUserInfoError,
+    exercises,
+    tabHeaders: exerciseTabHeaders,
+  };
 };
 
 export default useExerciseChoice;
