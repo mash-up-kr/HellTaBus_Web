be30e999c3f9c2b4ea2fb3601955a9ab2eebc3ff
Kim-Jaemin420 <jany240@naver.com>
2021-12-01 18:25:44 +0900

    Feat: user 정보 patch 하는 api

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,10 +1,19 @@
 import api from '@/api/core';
-import { User } from '@/types';
+import { User, SurveyFields } from '@/types';
 import { throwErrorMessage } from '@/utils';
 
 export const getUserInfo = (): Promise<User> =>
   api
     .get({
       url: '/user/login-info',
+    })
+    .catch(throwErrorMessage);
+
+export const patchUserInfo = (userInfo: SurveyFields) =>
+  api
+    .patch({
+      headers: { 'Content-Type': 'application/json' },
+      url: '/user/my/base-information',
+      data: userInfo,
     })
     .catch(throwErrorMessage);
