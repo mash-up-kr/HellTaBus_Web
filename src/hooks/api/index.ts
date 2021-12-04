443ae77679f3c0bda9df6e52af5f066c97cf411d
Kim-Jaemin420 <jany240@naver.com>
2021-12-04 23:10:18 +0900

    Refactor: export userPatchUserInfo

--- src/hooks/api/index.ts
+++ src/hooks/api/index.ts
@@ -2,3 +2,4 @@ export { default as useFetchExercises } from './useFetchExercises';
 export { default as useFetchExerciseSuggestion } from './useFetchExerciseSuggestion';
 export { default as useFetchUserInfo } from './useFetchUserInfo';
 export { default as useFetchExerciseHistory } from './useFetchExerciseHistory';
+export { default as usePatchUserInfo } from './usePatchUserInfo';
