ad012fb69665b007442f54b39ea0ae981f2907f9
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-12-04 18:36:49 +0900

    Refactor: CustomButton Props의 네이밍을 명시적으로 변경

--- src/components/common/CustomButton/CustomButton.tsx
+++ src/components/common/CustomButton/CustomButton.tsx
@@ -1,29 +1,30 @@
 /* eslint-disable react/button-has-type */
 import React, { ReactNode } from 'react';
 import classNames from 'classnames';
-import style from './customeButton.module.scss';
+import style from './customButton.module.scss';
 
 interface Props {
-  id: string;
-  buttonType: 'button' | 'submit';
-  value?: string | number;
-  onClick: () => void;
-  isDisabled: boolean;
+  CustomButtonType: 'button' | 'submit';
+  handleClickCustomEvent: () => void;
+  isDisabled?: boolean;
   children: ReactNode;
 }
 
 const { s_nextButton, s_submitButton } = style;
 
-const CustomButton = ({ id, buttonType, value, onClick, isDisabled, children }: Props) => {
+const CustomButton = ({
+  CustomButtonType,
+  handleClickCustomEvent,
+  isDisabled,
+  children,
+}: Props) => {
   return (
     <button
-      id={id}
-      type={buttonType}
-      value={value}
-      onClick={onClick}
+      type={CustomButtonType}
+      onClick={handleClickCustomEvent}
       disabled={isDisabled}
       className={classNames(s_nextButton, {
-        [s_submitButton]: buttonType === 'submit',
+        [s_submitButton]: CustomButtonType === 'submit',
       })}
     >
       {children}
