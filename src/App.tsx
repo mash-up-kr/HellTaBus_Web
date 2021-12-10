5125391b6ff79c0b72ac53cb0300c6da32db2f3e
HaJunRyu <hjr4557@naver.com>
2021-12-10 19:57:07 +0900

    Refactor: 로딩 UI 개별 처리를 위한 suspense 제거
    Changes to be committed:
    modified:   src/App.tsx
    modified:   src/hooks/api/useFetchExerciseHistory.ts
    modified:   src/hooks/api/useFetchExerciseSuggestion.ts
    modified:   src/hooks/api/useFetchExercises.ts
    modified:   src/hooks/api/useFetchUserInfo.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import {
   EXERCISE_CHOICE_PAGE,
@@ -9,22 +9,19 @@ import {
   SPLIT_TYPE_CHAGNE_PAGE,
 } from '@/consts/route';
 import { Home, Survey, ExerciseChoice, ExerciseRoutine, NotFound, SplitTypeChange } from '@/pages';
-import { Loading } from '@/components';
 
 const App = () => {
   return (
     <>
-      <Suspense fallback={<Loading />}>
-        <Switch>
-          <Route exact path={HOME_PAGE} component={Home} />
-          <Route path={SURVEY_PAGE} component={Survey} />
-          <Route path={EXERCISE_CHOICE_PAGE} component={ExerciseChoice} />
-          <Route path={EXERCISE_ROUTINE_PAGE} component={ExerciseRoutine} />
-          <Route path={SPLIT_TYPE_CHAGNE_PAGE} component={SplitTypeChange} />
-          <Route path={NOT_FOUND_PAGE} component={NotFound} />
-          <Redirect to={NOT_FOUND_PAGE} />
-        </Switch>
-      </Suspense>
+      <Switch>
+        <Route exact path={HOME_PAGE} component={Home} />
+        <Route path={SURVEY_PAGE} component={Survey} />
+        <Route path={EXERCISE_CHOICE_PAGE} component={ExerciseChoice} />
+        <Route path={EXERCISE_ROUTINE_PAGE} component={ExerciseRoutine} />
+        <Route path={SPLIT_TYPE_CHAGNE_PAGE} component={SplitTypeChange} />
+        <Route path={NOT_FOUND_PAGE} component={NotFound} />
+        <Redirect to={NOT_FOUND_PAGE} />
+      </Switch>
     </>
   );
 };
