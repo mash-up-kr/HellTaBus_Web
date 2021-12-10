69ceaab2af244aa2463361770a0f106d0d1ca6d0
HaJunRyu <hjr4557@naver.com>
2021-12-10 19:48:59 +0900

    Refactor: 로딩 인디케이터 gif -> json 로띠 형식으로 변경
    gif 로딩 시간때문에 잠시 보이지 않는 이슈로 인한 리팩토링
    
    Changes to be committed:
    deleted:    src/assets/images/loading-indicator.gif
    new file:   src/assets/lottie/loading-animation.json
    modified:   src/components/common/Loading/Loading.tsx
    modified:   src/components/common/Loading/loading.module.scss

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import classNames from 'classnames';
+import Lottie from 'react-lottie';
 import style from './loading.module.scss';
-import loadingIndicator from '@/assets/images/loading-indicator.gif';
+import loadingAnimation from '@/assets/lottie/loading-animation.json';
 
 const { s_loadingBackground, s_loadingIndicator } = style;
 
 const Loading = () => {
+  const lottieOptions = {
+    animationData: loadingAnimation,
+  };
   return (
     <div className={classNames(s_loadingBackground)}>
-      <img
-        src={loadingIndicator}
-        alt="loading indicator"
-        className={classNames(s_loadingIndicator)}
-      />
+      <div className={classNames(s_loadingIndicator)}>
+        <Lottie options={lottieOptions} width={50} height={50} />
+      </div>
     </div>
   );
 };
